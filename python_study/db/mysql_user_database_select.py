aedb7b78a09cd7564578e206452b7aea94d52848
kevin <kevin.lee@mangoplate.com>
2018-03-31 17:53:50 +0900

    오늘 금일 스터디 한거 커밋

--- python_study/db/mysql_user_database_select.py
+++ python_study/db/mysql_user_database_select.py
@@ -2,7 +2,7 @@
 import pymysql
 
 # MySQL Connection 연결 (pymysql.cursors.DictCursor는 딕셔너리 형태로 출력되고 이 부분을 생략하면 결과가 튜플 형태로 출력)
-conn = pymysql.connect(host='localhost', user='root', password='12345678', db='korea', charset='utf8',
+conn = pymysql.connect(host='localhost', user='kevin', password='', db='korea', charset='utf8',
                        cursorclass=pymysql.cursors.DictCursor)
 
 # Connection 으로부터 Cursor 생성
